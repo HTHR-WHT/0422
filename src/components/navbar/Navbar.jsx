77242c4e3c40564afd7e09d7f9e2d237ebde215d
Heather White <miss.pomykal@gmail.com>
2022-04-04 20:18:50 -0400

    INSTALLS: mui icons

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,18 @@
 import "./navbar.scss";
-// import GitHubIcon from '@mui/icons-material/GitHub';
-// import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className="logo">howdy.</a>
+          <a href="#intro" className="logo">
+            howdy.
+          </a>
           <div className="itemContainer">
-          {/* <LinkedInIcon />
-          <GitHubIcon /> */}
+            <LinkedInIcon />
+            <GitHubIcon />
           </div>
         </div>
         <div className="right"></div>
